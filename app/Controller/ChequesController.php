636f38832b9ff0930129ce068f5dcf9dea51392e
bet1aguilar <betmart0901@gmail.com>
2014-01-07 21:14:02 -0430

    :D
    Feliz

--- app/Controller/ChequesController.php
+++ app/Controller/ChequesController.php
@@ -30,7 +30,7 @@ class ChequesController extends AppController {
                                             GROUP BY cobrado
                                             ORDER BY COBRADO");
                //debug($sumas);
-               //jose y bet son novios y jose es hermoso
+               //jose y bet son novios cambie nuevamente
                 if($this->data){  
                     if ($this->data['Cheque']['search_text']) { 
                         $this->set('cheques',  
